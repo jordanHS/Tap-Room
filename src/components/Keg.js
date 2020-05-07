b4c55e4aea643780863bc78c0f2f2922c771a795
Jordan Safford <jordansafford@gmail.com>
2020-05-07 15:47:07 -0700

    build ticket component

---
+++ src/components/Keg.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Keg(props) {
+    return (
+        <React.Fragment>
+            <div onClick = {() => props.whenKegClicked(props.id)}>
+                <h2>{props.name}</h2>
+                <h3>{props.brand}</h3>
+                <h3>{props.price}</h3>
+                <h3>{props.alcohol}</h3>
+            </div>
+        </React.Fragment>
+    );
+}
+
+Keg.propTypes = {
+    name: PropTypes.string,
+    brand: PropTypes.string,
+    price: PropTypes.string,
+    alcohol: PropTypes.string,
+    id: PropTypes.string,
+    whenKegClicked: PropTypes.func
+}
